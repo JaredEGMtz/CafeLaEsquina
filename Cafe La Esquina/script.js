69cbb6f8e00f75f5f715d0884ba720ef9d0e1b5e
oscar5168 <150090746+oscar5168@users.noreply.github.com>
2025-08-15 16:36:30 -0600

    Add files via upload

--- Cafe La Esquina/script.js
+++ Cafe La Esquina/script.js
@@ -1,8 +1,8 @@
-        // JavaScript para manejar el menú desplegable en móvil
-        const toggleButton = document.getElementById('toggle-button');
-        const navbarLinks = document.getElementById('navbar-links');
+// JavaScript para manejar el menú desplegable en móvil
+const toggleButton = document.getElementById('toggle-button');
+const navbarLinks = document.getElementById('navbar-links');
 
-        toggleButton.addEventListener('click', () => {
+toggleButton.addEventListener('click', () => {
             // Alterna la clase 'active' para mostrar/ocultar los enlaces
             navbarLinks.classList.toggle('active');
-        });
+});
